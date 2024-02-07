103252544b2e4c047635ccac20a70e6b0a5ecef1
Reuben Remy <reubenremy@gmail.com>
2024-02-07 13:05:08 -0500

    working with assertions

---
+++ main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x: i32 = 3;
+    // let y: i32;
+
+    assert_eq!(x,5);
+    println!("Success!");
+
+    let mut z = 1;
+    x = x + 2; // 3 
+
+    // Most of the time type declarations aren't necessary
+}
